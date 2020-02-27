3f99df6afa54894b6360c40be1c6a7d7b8585a15
Khachatur Melkonyan <khachatur.melkonyan.95@gmail.com>
2020-02-27 15:12:56 +0400

    Add project files.

---
+++ Entity_FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Entity_FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+        }
+    }
+}
